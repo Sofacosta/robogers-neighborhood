691b1136f747cd35c857a15a0f488bb689cd4928
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-22 16:48:28 -0600

    print number submitted by user

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function(event) {
     event.preventDefault();
     $("#result").show();
     const inputNumber = parseInt($("input#userNumber").val());
-    console.log(inputNumber);
-    //let inputString = inputNumber.split("");
+    $("p").html(inputNumber);
   });
 });
