1ad27e932829642aa15c6b87e62e1d2d533703f4
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-22 16:55:53 -0600

    split the numbers submitted into a string

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function(event) {
   $("form#formInput").submit(function (event) {
     event.preventDefault();
     $("#result").show();
-    const inputNumber = parseInt($("input#userNumber").val());
-    $("p").html(inputNumber);
+    const inputNumber = $("input#userNumber").val();
+    let inputString = inputNumber.split("");  
+    console.log(inputString);
+    $("p").html(inputString);
   });
 });
