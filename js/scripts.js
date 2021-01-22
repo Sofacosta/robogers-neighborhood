0039dee2ad60403b0f47a7b8a9cd23febb165852
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-22 16:41:32 -0600

    fix syntax error and make button work when submitting number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
-  $("#formInput").submit(function (event) {
+$(document).ready(function(event) {
+  $("form#formInput").submit(function (event) {
     event.preventDefault();
     $("#result").show();
-    const inputNumber = parseInt ($("input#number").val());
-
-    
+    const inputNumber = parseInt($("input#userNumber").val());
+    console.log(inputNumber);
+    //let inputString = inputNumber.split("");
+  });
+});
