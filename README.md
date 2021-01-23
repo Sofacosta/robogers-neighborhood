47a639b69753b4576b4d5cc77d7596d3e6079cf2
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-22 18:07:06 -0600

    add a little basic style to the page

--- README.md
+++ README.md
@@ -7,15 +7,15 @@
 ## SPECS
 Describe: beepBop()
 Test: "It should return an array of numbers inputted by the user including the number 0"
-Expect(beepBoop(8)).toEqual(["0,1,2,3,4,5,6,7,8"]);
+Expect(beepBoop(08234)).toEqual(["08234"]);
 
 Describe: beepBop()
 Test:"It should return "Beep!" when number 1 is inputted in any given number" 
-Expect(beepBoop(12)).toEqual(["0,Beep!,2,3,4,5,6,7,8,9,10,11,12"])
+Expect(beepBoop(1)).toEqual(["Beep!"])
 
 Describe: beepBop()
 Test:"It should return "Boop!" when number 2 is inputted in any given number"
-Expect(beepBop(12)).toEqual(["0,1,"Boop!,3,4,5,6,7,8,9,10"])
+Expect(beepBop(2)).toEqual(["Boop!"])
 
 Describe: beepBop()
 Test:"It should return "Won't you be my neighbor?" when number 3 is inputted in any given number"
--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,29 @@ $(document).ready(function(event) {
     event.preventDefault();
     $("#result").show();
     const inputNumber = $("input#userNumber").val();
-    let inputString = inputNumber.split("");  
-    const number1 = ["1"];
-    const number2 = ["2"];
-    const number3 = ["3"];
-    for (let i= 0; i < inputString.length; i += 1)
-    if (number1.includes(inputString[i])) {
-      inputString[i] = "Beep!";
-    }
+    let inputString = inputNumber.split(""); 
 
-    for (let i= 0; i < inputString.length; i += 1)
-    if (number2.includes(inputString[i])) {
-      inputString[i] = "Boop!";
-    }
+    
 
-    for (let i= 0; i < inputString.length; i += 1)
-    if (number3.includes(inputString[i])) {
-      inputString[i] = "Won't you be my neighbor?";
-    }
+    
+    
+    // const number1 = ["1"];
+    // const number2 = ["2"];
+    // const number3 = ["3"];
+    // for (let i= 0; i < inputString.length; i += 1)
+    // if (number1.includes(inputString[i])) {
+    //   inputString[i] = "Beep!";
+    // }
+
+    // for (let i= 0; i < inputString.length; i += 1)
+    // if (number2.includes(inputString[i])) {
+    //   inputString[i] = "Boop!";
+    // }
+
+    // for (let i= 0; i < inputString.length; i += 1)
+    // if (number3.includes(inputString[i])) {
+    //   inputString[i] = "Won't you be my neighbor?";
+    // }
 
     $("p").html(inputString);
   });
